6b8444b9eb350e0f2e61996d6d6f8ebfe1b9e9d1
halitklyc <halitenes.kalayci@gmail.com>
2025-03-02 14:15:49 +0300

    .

--- oop.py
+++ oop.py
@@ -2,16 +2,30 @@
 # nesne yönelimli programlama
 
 class Car():
-    model = ""
+    def __init__(self, model):
+        self.__model = model
+        print("Yeni car üretildi.")
 
     def start(self):
-        model = "Toyota"
-        print(f"{self.model} araba başlatılıyor..")
+        print(f"{self.__model} araba başlatılıyor..")
 
     def increase_speed(self, amount):
         print(f"Hız şu kadar artırılıyor: {amount}")
+    
+    # getter
+    def get_model(self):
+        return self.__model
+    # setter
+    def set_model(self,model):
+        if len(model) < 3:
+            print("Marka ismi 3 haneden küçük olamaz")
+            return
+        self.__model = model
 
-car = Car()   # instance
-car.model = "Hyundai"
+car = Car("Hyundai") # instance
+car.set_model("a")
 car.start() 
 car.increase_speed(50)
+
+car1 = Car("Honda") # ilgili classın yapıcı methodunu çağırır (constructor)
+car1.start()
